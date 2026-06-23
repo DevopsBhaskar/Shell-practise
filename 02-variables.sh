d7acbd1ea4005ec9657c5fbd3af858fb0df35347
DevopsBhaskar <bhaskara.rao.dev@gmail.com>
2026-06-23 21:07:36 +0530

    updating

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,9 +4,13 @@ PERSON="Bhaskar"
 echo "Person name is $PERSON"
 
 per1=$1
+
 per2=$2
 
 echo "$per1:: How are you, $per2"
+
 echo "I am good $per2, what about you $per1"
+
 echo "$per1, how is your devops daily job"
+
 echo "$per2, its interesting, $per1"
