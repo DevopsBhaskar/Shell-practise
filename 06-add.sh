e640cb95737f80eeeb72c299b7a95e253c486da3
DevopsBhaskar <bhaskara.rao.dev@gmail.com>
2026-06-23 21:39:16 +0530

    updating

--- 06-add.sh
+++ 06-add.sh
@@ -2,9 +2,9 @@
 
 echo "adding two numbers"
 read s1
-echo "first number is:$s1
+echo "first number is:$s1"
 read s2
-echo "second number is:$s2
+echo "second number is:$s2"
 
 sum=$((s1+s2))
 echo "total sum is $sum"
