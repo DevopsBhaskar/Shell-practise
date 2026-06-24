9ca57d5b351b161451bd733f317fe12763697a0c
DevopsBhaskar <bhaskara.rao.dev@gmail.com>
2026-06-24 05:48:03 +0530

    updating

---
+++ 10-installations.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -nq 0 ]; then
+    echo "ERROR: Please run with sudo previliages"
+fi
+
+dnf install mysqld -y
+
+if [ $? -eq 0 ]; then
+    echo "MYSQL is installed"
+else
+    echo "SOMETHING is issue with MYSQLD"
+fi
